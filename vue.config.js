1cfc7355f68ab67b8c6765f25e8bdbc2b561cd09
Adil320 <54187988+Adil320@users.noreply.github.com>
2022-03-11 22:44:21 +0600

    publiccath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: "/https://charmium.ru/",
   transpileDependencies: true,
   devServer: {
     proxy: 'https://charmium.ru/',
