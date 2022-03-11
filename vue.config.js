70d058d1d06f690867cc02bd3a458f370d489da8
Adil320 <54187988+Adil320@users.noreply.github.com>
2022-03-11 21:25:17 +0600

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/user/',
+  publicPath: process.env.NODE_ENV === "production" ? "/USER/" : "/",
   transpileDependencies: true,
   devServer: {
     proxy: 'https://charmium.ru/',
